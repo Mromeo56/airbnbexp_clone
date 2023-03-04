47de4cdf7391c41d77814af2e7616af501e7d249
Romeo <romeo.moreaux35@gmail.com>
2023-03-04 15:21:47 +0100

    Real first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+    return (
+      <nav className="Navbar">
+        <img src="src\assets\airbnb_logo.png" className="nav--logo"/>
+      </nav>
+    )
+  }
+  
+  export default Navbar
